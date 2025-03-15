5dc2bcbf6b7babcd3daae911c0aaa0c6f6d6d531
ryandioneda <er.dioneda@gmail.com>
2025-03-15 15:05:30 -0400

    Removed debugging statements

--- backend/src/apps/authentication/signals.py
+++ backend/src/apps/authentication/signals.py
@@ -9,21 +9,15 @@ User = get_user_model()
 @receiver(post_save, sender=EmailAddress)
 def update_user_verification_status(sender, instance, **kwargs):
     """Update the User model's is_verified field when an email is verified."""
-    print("CALLED A SIGNAL")
     if instance.verified:  # If email is marked as verified
         user = instance.user
-        print(f"Before update: is_verified={user.is_verified}, is_vendor={user.is_vendor}")
         if user and not user.is_verified:
             user.is_verified = True
             user.save(update_fields=["is_verified"])
-            print(f"After update: is_verified={user.is_verified}, is_vendor={user.is_vendor}")
 
-#! not causing the vendor update issue
-# @receiver(m2m_changed, sender=User.groups.through)
-# def update_is_vendor(sender, instance, action, **kwargs):
-#     """Update the User mode's is_vendor when a user is added to Vendor group"""
-#     print(f"Signal Triggered!")
-#     if action in ["post_add", "post_remove", "post_clear"]:
-#         instance.is_vendor = instance.groups.filter(name="Vendor").exists()
-#         instance.save()
-#         print("UPDATED USER TO VENDOR")
+@receiver(m2m_changed, sender=User.groups.through)
+def update_is_vendor(sender, instance, action, **kwargs):
+    """Update the User mode's is_vendor when a user is added to Vendor group"""
+    if action in ["post_add", "post_remove", "post_clear"]:
+        instance.is_vendor = instance.groups.filter(name="Vendor").exists()
+        instance.save()

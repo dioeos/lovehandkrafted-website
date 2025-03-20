c24f32883f93c8ec6ca5cc9fa3a819578b8232a9
ryandioneda <er.dioneda@gmail.com>
2025-03-20 17:43:30 -0400

    Created Product model

--- backend/src/apps/products/models.py
+++ backend/src/apps/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Product(models.model):
+    """Represents an individual product item"""
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    default_price = models.CharField(max_length=100, blank=True, null=True)
+    thumbnail = models.URLField(blank=True, null=True)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        order = ("-created_at",)
+
+    def __str__(self):
+        return self.name
+    
+

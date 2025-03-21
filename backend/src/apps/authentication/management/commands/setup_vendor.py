ac9f0c185ab57685f3234a5f7ea67e1a932f1499
ryandioneda <er.dioneda@gmail.com>
2025-03-20 22:41:10 -0400

    Edited setup_vendor to wrap add_products permission in Products app

--- backend/src/apps/authentication/management/commands/setup_vendor.py
+++ backend/src/apps/authentication/management/commands/setup_vendor.py
@@ -4,6 +4,8 @@ Django command to set up the Vendor group on server startup
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from apps.products.models import Product
 
 class Command(BaseCommand):
 
@@ -24,9 +26,15 @@ class Command(BaseCommand):
 
         for perm_codename in permissions_to_add:
             try:
-                perm = Permission.objects.get(codename=perm_codename)
-                vendor_group.permissions.add(perm)
-                self.stdout.write(self.style.SUCCESS(f"Successfully assigned `{perm_codename}` to Vendor group"))
+
+                if perm_codename == "add_product":
+                    # Ensure we fetch it from the products app
+                    product_content_type = ContentType.objects.get_for_model(Product)
+                    perm = Permission.objects.get(codename=perm_codename, content_type=product_content_type)
+                    self.stdout.write(self.style.SUCCESS(f"Successfully assigned `{perm_codename}` to Vendor group"))
+                else:
+                    perm = Permission.objects.get(codename=perm_codename)
+                    self.stdout.write(self.style.SUCCESS(f"Successfully assigned `{perm_codename}` to Vendor group"))
             except Permission.DoesNotExist:
                 self.stdout.write(self.style.ERROR(f"Failed to assign `{perm_codename}`: Permission does not exist"))
 

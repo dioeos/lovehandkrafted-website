656b4e0599db3d61bfbc7a9f3ba94116aa4adc62
ryandioneda <er.dioneda@gmail.com>
2025-05-21 00:25:04 -0400

    update

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,16 +1,39 @@
-import Layout from "../../components/Layout/Layout";
+import Nav from "../../components/Nav/Nav";
+import { Card } from "../../components/Card/Card";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Footer from "../../components/Footer/Footer";
+import { Extras } from "../Index/Extras";
 
 const Contact = () => {
-    console.log("Contact")
-    return (
-    <Layout>
-        <div id="contact-root" className="h-screen bg-purple-500">
+  const circleContainer = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: circleContainer,
+    offset: ["start end", "end start"],
+  });
 
+  const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
+
+  return (
+    <div className="bg-[#352f36]">
+      <Nav />
+      <div id="contact-wrapper" className="min-h-screen bg-[#FAF9F6]">
+        <div id="contact-container" className="mt-[4em] mx-[2vw] p-4">
+          <div id="contact-header">
+            <h1 className="uppercase !text-[4rem] md:!text-[6rem] text-[#352f36] satoshi">
+              CONTACT US
+            </h1>
+          </div>
         </div>
-    </Layout>
+      </div>
 
-    )
+      <div className="overflow-x-hidden overflow-y-hidden bg-[#352f36]">
+        <Extras showSlider={false} />
+      </div>
 
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default Contact;
--- frontend/src/pages/Shop/Shop.jsx
+++ frontend/src/pages/Shop/Shop.jsx
@@ -1,70 +1,59 @@
 import Layout from "../../components/Layout/Layout";
 import Nav from "../../components/Nav/Nav";
 import { Card } from "../../components/Card/Card";
-import { useRef } from 'react';
+import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Footer from "../../components/Footer/Footer";
 import { Extras } from "../Index/Extras";
 
 const Shop = () => {
+  const circleContainer = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: circleContainer,
+    offset: ["start end", "end start"],
+  });
 
-    const circleContainer = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: circleContainer,
-        offset: ["start end", "end start"]
-    })
+  const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
 
+  return (
+    <div className="bg-[#352f36]">
+      <Nav />
+      <div id="shop-wrapper" className="min-h-screen bg-[#FAF9F6]">
+        <div id="shop-container" className="mt-[4em] mx-[2vw] p-4">
+          <div id="shop-header">
+            <h1 className="uppercase !text-[4rem] md:!text-[6rem] text-[#352f36] satoshi">
+              ALL
+            </h1>
+          </div>
 
-    const height = useTransform(scrollYProgress, [0, 0.9], [50, 0])
+          <div id="filter-system-container"></div>
 
-    
-    return (
-        <div className="bg-[#352f36]">
-            <Nav/>
-            <div id="shop-wrapper" className="min-h-screen bg-[#FAF9F6]">
+          <div id="shop-grid" className="grid grid-cols-2 md:grid-cols-4">
+            <Card productName="Item 1" />
+            <Card productName="Item 2" />
+            <Card productName="Item 3" />
+            <Card productName="Item 4" />
 
-                <div id="shop-container" className="mt-[4em] mx-[2vw] p-4">
-                    <div id="shop-header">
-                        <h1 className="uppercase !text-[4rem] md:!text-[6rem] text-[#352f36] satoshi">ALL</h1>
-                    </div>
+            <Card productName="Item 1" />
+            <Card productName="Item 2" />
+            <Card productName="Item 3" />
+            <Card productName="Item 4" />
 
-                    <div id="filter-system-container">
-                    </div>
-
-                    <div id="shop-grid" className="grid grid-cols-2 md:grid-cols-4">
-
-                        <Card productName="Item 1"/>
-                        <Card productName="Item 2"/>
-                        <Card productName="Item 3"/>
-                        <Card productName="Item 4"/>
-
-                        <Card productName="Item 1"/>
-                        <Card productName="Item 2"/>
-                        <Card productName="Item 3"/>
-                        <Card productName="Item 4"/>
-
-
-                        <Card productName="Item 1"/>
-                        <Card productName="Item 2"/>
-                        <Card productName="Item 3"/>
-                        <Card productName="Item 4"/>
-
-                    </div>
-                </div>
-
-
-                <div className="overflow-x-hidden overflow-y-hidden bg-[#352f36]">
-                    <Extras showSlider={false}/>
-                </div>
-
-
-
-                <Footer/>
-
-            </div>
+            <Card productName="Item 1" />
+            <Card productName="Item 2" />
+            <Card productName="Item 3" />
+            <Card productName="Item 4" />
+          </div>
         </div>
-    )
 
-}
+        <div className="overflow-x-hidden overflow-y-hidden bg-[#352f36]">
+          <Extras showSlider={false} />
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Shop;

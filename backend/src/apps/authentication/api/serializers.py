72f6a9e21fe8cff0642e9bbd97f6440b501e651c
ryandioneda <er.dioneda@gmail.com>
2025-05-21 01:20:29 -0400

    Authentication app security edits
    Changes:
    - Edited register serializer to use validated data instead of raw JSON payload
    - Edited password validation
    - Normalized emails (User@Example.com == user@example.com) in db

--- backend/src/apps/authentication/api/serializers.py
+++ backend/src/apps/authentication/api/serializers.py
@@ -8,8 +8,12 @@ from allauth.account.models import EmailAddress
 from django.contrib.auth import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from dj_rest_auth.serializers import LoginSerializer
-from django.contrib.auth import (get_user_model,)
+from django.contrib.auth import (
+    get_user_model,
+)
+from django.contrib.auth.password_validation import validate_password
 import re
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for user object"""
@@ -17,7 +21,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ["email", "password", "first_name", "last_name", "is_vendor"]
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {
+            "password": {"write_only": True, "validators": [validate_password]}
+        }
 
     def create(self, validated_data):
         """Create a user with encrypted password and return the user"""
@@ -34,45 +40,58 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+
 class CustomRegisterSerializer(RegisterSerializer):
     """Custom serializer for user registration."""
-    username = None #remove username
+
+    username = None  # remove username
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(max_length=255, required=True)
     last_name = serializers.CharField(max_length=255, required=True)
 
     @transaction.atomic
     def save(self, request):
+        # user.first_name = self.data.get("first_name", "")
+        first_name = self.validated_data["first_name"]
+        # user.last_name = self.data.get("last_name", "")
+        last_name = self.validated_data["last_name"]
+        email = self.validated_data["email"]
+
+        # normalize the email to prevent dupes / case sensitive
+        email = get_user_model().objects.normalize_email(email)
+        self.validated_data["email"] = email
+
         user = super().save(request)
-        user.first_name = self.data.get("first_name", "")
-        user.last_name = self.data.get("last_name", "")
-        user.save()
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save(update_fields=["first_name", "last_name"])
         return user
-    
-    
+
     def validate_email(self, email):
         email = email.strip()  # Remove leading/trailing spaces
+        email = get_user_model().objects.normalize_email(email)
 
         # Validate email format
-        email_regex = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
+        email_regex = r"^[^\s@]+@[^\s@]+\.[^\s@]+$"
         if not re.match(email_regex, email):
             raise ValidationError({"email": ["Please enter a valid email address."]})
 
         if get_user_model().objects.filter(email=email).exists():
-            raise ValidationError({"email": ["An account with this email already exists"]})
+            raise ValidationError(
+                {"email": ["An account with this email already exists"]}
+            )
 
         return email
-            
+
 
 class CustomLoginSerializer(LoginSerializer):
     username_field = "email"
 
-
-    def get_auth_user(self, request, email, password): 
+    def get_auth_user(self, request, email, password):
         """Authenticate user with email and password."""
         user = authenticate(email=email, password=password)
         if not user:
-            #drf translates into 401 response
+            # drf translates into 401 response
             raise AuthenticationFailed("Invalid credentials")
         return user
 
@@ -80,12 +99,11 @@ class CustomLoginSerializer(LoginSerializer):
         email = attrs.get(self.username_field)
         password = attrs.get("password")
 
-        user = self.get_auth_user(self.context.get('request'), email, password)
+        user = self.get_auth_user(self.context.get("request"), email, password)
         if user and not EmailAddress.objects.filter(user=user, verified=True).exists():
-            #drf translates into 401 response
+            # drf translates into 401 response
             raise AuthenticationFailed("Your email is not verified.")
-        
+
         data = super().validate(attrs)
         data["is_vendor"] = user.is_vendor
         return data
-    
--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 from datetime import datetime, timedelta
 
-#from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,81 +36,75 @@ ALLOWED_HOSTS = ["backend", "localhost"]
 
 INSTALLED_APPS = [
     # ---- DEFAULT ----
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # ---- Django REST Framework ----
-    'rest_framework',
-    'rest_framework.authtoken',
-    'rest_framework_simplejwt.token_blacklist',
-    'drf_spectacular', #? for swagger documentation
-
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",  # ? for swagger documentation
     # ---- Django AllAuth ----
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    #"allauth.socialaccount.providers.google",
-    #"allauth.mfa",
+    # "allauth.socialaccount.providers.google",
+    # "allauth.mfa",
     # "allauth.headless",
     # "allauth.usersessions",
-
-
     # ---- Application Apps ----
-    'apps.authentication',
-    'apps.newsletter',
-    'apps.products',
-
+    "apps.authentication",
+    "apps.newsletter",
+    "apps.products",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware', #! must be present for admin application
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    #? ALLAUTH
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",  #! must be present for admin application
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # ? ALLAUTH
     "allauth.account.middleware.AccountMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': config("POSTGRES_DB_NAME"),
-        'USER': config("POSTGRES_USERNAME"),
-        'PASSWORD': config("POSTGRES_PASSWORD"),
-        'HOST': config("POSTGRES_HOST"),
-        'PORT': config("POSTGRES_PORT")
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB_NAME"),
+        "USER": config("POSTGRES_USERNAME"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
@@ -120,76 +114,72 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 9,
+        },
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 #! ADDED
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
-    
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        #if jwt access token is present -> authenticated
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        # if jwt access token is present -> authenticated
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ],
-
-    #? COMMENTED
+    # ? COMMENTED
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ]
 }
 
 REST_AUTH = {
-    'REGISTER_SERIALIZER': "apps.authentication.api.serializers.CustomRegisterSerializer",
-    'USER_DETAILS_SERIALIZER': 'apps.authentication.api.serializers.UserSerializer',
-    'LOGIN_SERIALIZER': "apps.authentication.api.serializers.CustomLoginSerializer",
+    "REGISTER_SERIALIZER": "apps.authentication.api.serializers.CustomRegisterSerializer",
+    "USER_DETAILS_SERIALIZER": "apps.authentication.api.serializers.UserSerializer",
+    "LOGIN_SERIALIZER": "apps.authentication.api.serializers.CustomLoginSerializer",
     "SESSION_LOGIN": False,
-
     "USE_JWT": True,
     "JWT_AUTH_COOKIE": "jwt-access-token",
     "JWT_AUTH_REFRESH_COOKIE": "jwt-refresh-token",
     "JWT_AUTH_SECURE": True,
-    "JWT_AUTH_HTTPONLY": True, 
+    "JWT_AUTH_HTTPONLY": True,
 }
 
 REST_AUTH_SERIALIZERS = {
-    'PASSWORD_RESET_SERIALIZER': 'apps.authentication.api.serializers.CustomPasswordResetSerializer'
+    "PASSWORD_RESET_SERIALIZER": "apps.authentication.api.serializers.CustomPasswordResetSerializer"
 }
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-
-    #? ADDED
-    'SIGNING_KEY': config('JWT_SIGNING_KEY')
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    # ? ADDED
+    "SIGNING_KEY": config("JWT_SIGNING_KEY"),
 }
-
-
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -199,12 +189,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #! Added
 # CSRF_COOKIE_SAMESITE = "Strict"
@@ -212,36 +202,27 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CSRF_COOKIE_HTTPONLY = True
 # SESSION_COOKIE_HTTPONLY = True
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost',
+    "http://localhost",
     #'https://productionurl.com'
 ]
 
-#* Prod only
+# * Prod only
 # CSRF_COOKIE_SECURE = True
 # SESSION_COOKIE_SECURE = True
 
 SITE_ID = 1
 
 
-AUTHENTICATION_BACKENDS = ([
-    #"allauth.account.auth_backends.AuthenticationBackend",
-    'django.contrib.auth.backends.ModelBackend',
-])
+AUTHENTICATION_BACKENDS = [
+    # "allauth.account.auth_backends.AuthenticationBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
 
 
-
-
-
-
-
-
-
-#? ADDED
+# ? ADDED
 SPECTACULAR_SETTINGS = {
     #'SERVE_PERMISSIONS': ['rest_framework.permissions.IsAdminUser'],  # Only admin users
-    'SERVE_AUTHENTICATION': [
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    ]
+    "SERVE_AUTHENTICATION": ["dj_rest_auth.jwt_auth.JWTCookieAuthentication"]
 }
 
 
@@ -250,32 +231,38 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('GOOGLE_SMTP_APP_EMAIL')
-EMAIL_HOST_PASSWORD = config('GOOGLE_SMTP_APP_PASS')
+EMAIL_HOST_USER = config("GOOGLE_SMTP_APP_EMAIL")
+EMAIL_HOST_PASSWORD = config("GOOGLE_SMTP_APP_PASS")
 
 # ---- Django Configuration ----
-AUTH_USER_MODEL = "authentication.User" #? custom user model
-LOGIN_URL = "/login" #? login URL for Django's authentication system
+AUTH_USER_MODEL = "authentication.User"  # ? custom user model
+LOGIN_URL = "/login"  # ? login URL for Django's authentication system
 
 # ---- dj-rest-auth Configuration ----
 FRONTEND_URL = "http://localhost"
 
 # ---- Django AllAuth Configuration ----
-EMAIL_CONFIRM_REDIRECT_BASE_URL = \
-    "http://localhost/email/confirm/"
+EMAIL_CONFIRM_REDIRECT_BASE_URL = "http://localhost/email/confirm/"
 
 
-PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL = \
+PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL = (
     "http://localhost/account/password-reset/confirm/"
+)
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-ACCOUNT_LOGIN_METHODS = ['email'] #? user logs in with email instead of username
-ACCOUNT_USERNAME_REQUIRED = False #? disables username field
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None #? removes username field from user model
-ACCOUNT_UNIQUE_EMAIL = True #? no duplicate emails
-ACCOUNT_CONFIRM_EMAIL_ON_GET = True #? confirms an email address when confirmation link is clicked
-ACCOUNT_EMAIL_CONFIRMATION_TEMPLATE = "templates/account/email_confirmation_message.txt" #? custom email template
-ACCOUNT_ADAPTER = "apps.authentication.adapters.CustomAccountAdapter" #? custom adapter logic
+ACCOUNT_LOGIN_METHODS = ["email"]  # ? user logs in with email instead of username
+ACCOUNT_USERNAME_REQUIRED = False  # ? disables username field
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None  # ? removes username field from user model
+ACCOUNT_UNIQUE_EMAIL = True  # ? no duplicate emails
+ACCOUNT_CONFIRM_EMAIL_ON_GET = (
+    True  # ? confirms an email address when confirmation link is clicked
+)
+ACCOUNT_EMAIL_CONFIRMATION_TEMPLATE = (
+    "templates/account/email_confirmation_message.txt"  # ? custom email template
+)
+ACCOUNT_ADAPTER = (
+    "apps.authentication.adapters.CustomAccountAdapter"  # ? custom adapter logic
+)
 
 EMAIL_SUBJECT_PREFIX = "[Lovehandkrafted] "

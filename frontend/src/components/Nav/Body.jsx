cc8f23ebc768c95d5dd62b3677e05e3eed012cd1
ryandioneda <er.dioneda@gmail.com>
2025-05-23 17:04:54 -0400

    Styled navbar for responsiveness

--- frontend/src/components/Nav/Body.jsx
+++ frontend/src/components/Nav/Body.jsx
@@ -1,52 +1,61 @@
-import { motion } from 'framer-motion';
-import { blur, translate } from './anim';
-import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
+import { blur, translate } from "./anim";
+import { Link } from "react-router-dom";
 
 const Body = ({ links, selectedLink, setSelectedLink }) => {
+  const getChars = (word) => {
+    if (!word) return [];
+    let chars = [];
+    word.split("").forEach((char, i) => {
+      chars.push(
+        <motion.span
+          custom={[i * 0.02, (word.length - i) * 0.01]}
+          variants={translate}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          key={char + i}
+        >
+          {char}
+        </motion.span>,
+      );
+    });
+    return chars;
+  };
 
-    const getChars = (word) => {
-        if (!word) return [];
-        let chars = [];
-        word.split("").forEach((char, i) => {
-            chars.push(
-                <motion.span
-                    custom={[i * 0.02, (word.length - i) * 0.01]}
-                    variants={translate} initial="initial"
-                    animate="enter"
-                    exit="exit"
-                    key={char + i}
-                >
-                    {char}
-                </motion.span>
-            );
-        });
-        return chars;
-    };
-
-    return (
-        <div className='flex flex-wrap mt-[40px] max-w-full md:max-w-[1200px] uppercase text-[#0B1215]'>
-            {
-                links.map((link, index) => {
-                    const { title, href } = link || {};
-                    return (
-                        <Link key={`l_${index}`} to={href} className='' style={{ textDecoration: "none", color: "#0B1215" }}>
-
-                            <motion.p
-                                onMouseOver={() => { setSelectedLink({ isActive: true, index }) }}
-                                onMouseLeave={() => { setSelectedLink({ isActive: false, index }) }}
-                                variants={blur}
-                                animate={selectedLink.isActive && selectedLink.index !== index ? "open" : "closed"}
-                                className={`m-0 flex flex-wrap overflow-hidden text-2xl md:text-4xl lg:text-7xl pr-[48px] pt-[10px] font-light ${title === "Shop" ? "satoshi-italic" : ""}`}
-                            >
-                                {getChars(title)}
-                            </motion.p>
-
-                        </Link>
-                    );
-                })
-            }
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap mt-[20px] max-w-full md:max-w-[1200px] uppercase text-[#0B1215]">
+      {links.map((link, index) => {
+        const { title, href } = link || {};
+        return (
+          <Link
+            key={`l_${index}`}
+            to={href}
+            className=""
+            style={{ textDecoration: "none", color: "#0B1215" }}
+          >
+            <motion.p
+              onMouseOver={() => {
+                setSelectedLink({ isActive: true, index });
+              }}
+              onMouseLeave={() => {
+                setSelectedLink({ isActive: false, index });
+              }}
+              variants={blur}
+              animate={
+                selectedLink.isActive && selectedLink.index !== index
+                  ? "open"
+                  : "closed"
+              }
+              className={`text-[#352f36] m-0 flex flex-wrap overflow-hidden text-3xl md:text-4xl lg:text-7xl pr-[32px] pt-[10px] font-light ${title === "Shop" ? "satoshi-italic" : ""}`}
+            >
+              {getChars(title)}
+            </motion.p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Body;
--- frontend/src/components/Nav/Footer.jsx
+++ frontend/src/components/Nav/Footer.jsx
@@ -2,69 +2,50 @@ import { translate } from "./anim";
 import { motion } from "framer-motion";
 
 const Footer = () => {
-    return (
-        <div id="footer" className="flex items-start flex-wrap uppercase mt-[20px] font-[12px]">
-            <ul className="list-none w-1/4 mt-[5px] overflow-hidden">
-                <motion.li
-                    custom={[0.3, 0]} 
-                    variants={translate}
-                    initial="initial"
-                    animate="enter"
-                    exit="exit"
-                >
-                    <span className="text-[#9f9689]">Made by: </span>Lovehandkrafted
-                </motion.li>
-            </ul>
+  return (
+    <div
+      id="footer"
+      className="flex items-start flex-wrap uppercase mt-[20px] text-[12px] md:text-[18px] flex-col md:flex-row md:gap-[5rem]"
+    >
+      <ul className="list-none mt-[5px] overflow-hidden p-0">
+        <motion.li
+          custom={[0.3, 0]}
+          variants={translate}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <span className="text-[#9f9689]">Made by: </span>
+          <span className="text-[#352f36]">Lovehandkrafted</span>
+        </motion.li>
+      </ul>
 
-            <ul className="list-none w-1/4 mt-[5px] overflow-hidden">
-                <motion.li
-                    custom={[0.3, 0]} 
-                    variants={translate}
-                    initial="initial"
-                    animate="enter"
-                    exit="exit"
-                >
-                    <span className="text-[#9f9689]">Made by: </span>Ryan Dioneda
-                </motion.li>
-            </ul>
+      <ul className="list-none mt-[5px] overflow-hidden  p-0">
+        <motion.li
+          custom={[0.3, 0]}
+          variants={translate}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <span className="text-[#9f9689]">Based in: </span>
+          <span className="text-[#352f36]">Manila, PH</span>
+        </motion.li>
+      </ul>
 
-            
-            <ul className="list-none w-1/4 mt-[5px] overflow-hidden">
-                <motion.li
-                    custom={[0.3, 0]} 
-                    variants={translate}
-                    initial="initial"
-                    animate="enter"
-                    exit="exit"
-                >
-                    <span className="text-[#9f9689]">Made by: </span>Ryan Dioneda
-                </motion.li>
-            </ul>
-
-            <ul className="list-none w-1/4 mt-[5px] overflow-hidden">
-                <motion.li
-                    custom={[0.3, 0]} 
-                    variants={translate}
-                    initial="initial"
-                    animate="enter"
-                    exit="exit"
-                >
-                    <span className="text-[#9f9689]">Made by: </span>Ryan Dioneda
-                </motion.li>
-
-                {/* <motion.li
-                    custom={[0.3, 0]} 
-                    variants={translate}
-                    initial="initial"
-                    animate="enter"
-                    exit="exit"
-                >
-                    <span className="text-[#9f9689]">Made by: </span>Ryan Dioneda
-                </motion.li> */}
-            </ul>
-
-        </div>
-    )
-
-}
+      <ul className="list-none mt-[5px] overflow-hidden  p-0">
+        <motion.li
+          custom={[0.3, 0]}
+          variants={translate}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <span className="text-[#9f9689]">Images by: </span>
+          <span className="text-[#352f36]">Maria Dioneda</span>
+        </motion.li>
+      </ul>
+    </div>
+  );
+};
 export default Footer;

a1358e8f62d6a71540858dba9317b888ca03e281
ryandioneda <er.dioneda@gmail.com>
2025-07-09 14:44:25 -0400

    Implemented r2 worker logic (w/ auth)

--- r2-worker/src/index.js
+++ r2-worker/src/index.js
@@ -1,15 +1,61 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const hasValidHeader = (request, env) => {
+	return request.headers.get('X-Custom-Auth-Key') === env.AUTH_KEY_SECRET;
+};
+
+const hasValidGetToken = (request, env) => {
+	return request.headers.get('X-Read-Token') === env.READ_ONLY_TOKEN;
+};
+
+function authorizeRequest(request, env, key) {
+	switch (request.method) {
+		case 'PUT':
+		case 'DELETE':
+			return hasValidHeader(request, env);
+		case 'GET':
+			return hasValidGetToken(request, env);
+		default:
+			return false;
+	}
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
+
+		if (!authorizeRequest(request, env, key)) {
+			return new Response('Forbidden', { status: 403 });
+		}
+
+		switch (request.method) {
+			case 'PUT':
+				await env.LHK_PRODUCT_BUCKET.put(key, request.body);
+				return new Response(`Put ${key} successfully`);
+
+			case 'GET':
+				const object = await env.LHK_PRODUCT_BUCKET.get(key);
+				if (object === null) {
+					return new Response('Object Not Found', { status: 404 });
+				}
+				const headers = new Headers();
+				object.writeHttpMetadata(headers);
+				headers.set('etag', object.httpEtag);
+
+				return new Response(object.body, {
+					headers,
+				});
+
+			case 'DELETE':
+				await env.LHK_PRODUCT_BUCKET.delete(key);
+				return new Response('Deleted!');
+
+			default:
+				return new Response('Method Not Allowed', {
+					status: 405,
+					headers: {
+						Allow: 'PUT, GET, DELETE',
+					},
+				});
+		}
 	},
 };

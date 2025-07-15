7fcbe6ae99587836fa272c2e767941f3cc8fbf24
ryandioneda <er.dioneda@gmail.com>
2025-07-15 14:27:31 -0400

    Added contact page styling and content

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 import Nav from "../../components/Nav/Nav";
-import { Card } from "../../components/Card/Card";
 import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Footer from "../../components/Footer/Footer";
@@ -15,14 +14,58 @@ const Contact = () => {
   const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
 
   return (
-    <div className="bg-[#352f36]">
+    <div className="">
       <Nav />
-      <div id="contact-wrapper" className="min-h-screen bg-[#FAF9F6]">
+
+      <div id="contact-wrapper" className="h-screen bg-[#FAF9F6]">
         <div id="contact-container" className="mt-[4em] mx-[2vw] p-4">
-          <div id="contact-header">
-            <h1 className="uppercase !text-[4rem] md:!text-[6rem] text-[#352f36] satoshi">
-              CONTACT US
-            </h1>
+          <div id="reset-content" className="w-full mx-auto p-4 mt-8">
+            <div className="text-center satoshi">
+              <h1 className="uppercase !text-[2.5rem] md:!text-[6rem] text-[#352f36] satoshi">
+                Contact Us
+              </h1>
+
+              <div
+                className="
+                  max-w-md
+                  w-full
+                  mx-auto
+                  text-sm sm:text-base
+                  text-[#352f36]
+                  satoshi
+                  mb-5
+                "
+              >
+                Thank you for exploring the Lovehandkrafted Store! Please
+                contact us for any questions, collaboration ideas, or custom
+                requests. We are always happy to connect and create something
+                special with you.
+              </div>
+
+              <div
+                className="
+                  max-w-md
+                  w-full
+                  mx-auto
+                  text-sm sm:text-base
+                  text-[#352f36]
+                  satoshi
+                  flex
+                  items-center
+                  justify-center
+                "
+              >
+                <a
+                  href="mailto:lovehandkrafted@gmail.com"
+                  className="border-1 rounded-lg border-[#352f36] p-1 inline-block"
+                  style={{ textDecoration: "none", color: "#352f36" }}
+                >
+                  lovehandkrafted@gmail.com
+                </a>
+              </div>
+            </div>
+
+            <div></div>
           </div>
         </div>
       </div>

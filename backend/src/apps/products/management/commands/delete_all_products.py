cfaee5f492ba4c2746c848b044f39e3396786eec
ryandioneda <er.dioneda@gmail.com>
2025-07-17 12:40:39 -0400

    Edited delete_all_products command to delete Cloudflare img

--- backend/src/apps/products/management/commands/delete_all_products.py
+++ backend/src/apps/products/management/commands/delete_all_products.py
@@ -1,9 +1,32 @@
 from django.core.management.base import BaseCommand
 from apps.products.models import Product
+from decouple import config
+import requests
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        Product.objects.all().delete()
+        products = Product.objects.all()
+
+        for product in products:
+            if product.thumbnail:
+                self.stdout.write(f"Product has thumbnail, deleting: {product.thumbnail}")
+                #delete logic
+                r2_worker_base_url = config("R2_WORKER_BASE_URL")
+                x_cust_auth_key = config("R2_WORKER_AUTH_KEY_SECRET")
+                key = product.thumbnail.replace(f"{r2_worker_base_url}/", "")
+                delete_url = f"{r2_worker_base_url}/{key}"
+
+                res = requests.delete(
+                    delete_url,
+                    headers={
+                        "X-Custom-Auth-Key": x_cust_auth_key
+                    }
+                )
+
+                if res.status_code != 200:
+                    self.stdout.write(self.style.ERROR(f"Failed to delete product thumbail: {delete_url}"))
+
+        products.delete()
 
         if Product.objects.all().count() == 0:
             self.stdout.write(self.style.SUCCESS("Successfully deleted all products."))

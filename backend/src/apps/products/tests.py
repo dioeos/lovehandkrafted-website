33750eff1a7e3b29fc280c0d16fdb2cde756c60a
ryandioneda <er.dioneda@gmail.com>
2025-07-22 11:00:38 -0400

    Added ProductTag and Products test

--- backend/src/apps/products/tests.py
+++ backend/src/apps/products/tests.py
@@ -22,3 +22,29 @@ class ProductModelTests(TestCase):
         self.assertTrue(self.product.active)
         self.assertIsNotNone(self.product.created_at)
         self.assertIsNotNone(self.product.updated_at)
+
+    def test_str(self):
+        self.assertEqual(str(self.product), "Sample Product")
+
+class ProductTagModelTests(TestCase):
+    def setUp(self):
+        self.product1 = Product.objects.create(name="P1")
+        self.product2 = Product.objects.create(name="P2")
+        self.tag = ProductTag.objects.create(name="Home")
+        self.tag.products.set([self.product1, self.product2])
+
+    def test_tag_creation(self):
+        self.assertEqual(self.tag.name, "Home")
+        self.assertTrue(isinstance(self.tag.id, UUID))
+
+    def test_relationship(self):
+        self.assertCountEqual(
+            self.tag.products.all(),
+            [self.product1, self.product2]
+        )
+
+        self.assertIn(self.tag, self.product1.tags.all())
+
+    def test_str(self):
+        self.assertEqual(str(self.tag), "Home")
+
